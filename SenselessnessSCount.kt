20bee384e8fd38e00798d79402cd65d5b0361289
Sakari Bansal <43090937+SK1dev@users.noreply.github.com>
2019-08-26 11:45:02 +0200

    Update SenselessnessSCount.kt

--- SenselessnessSCount.kt
+++ SenselessnessSCount.kt
@@ -3,11 +3,11 @@ fun main() {
     val numLetters = "senselessness".count({ letter -> letter == 's' })
     print(numLetters)
 }
-/*
+
 //SOLUTION 2
 fun main() {
     val numLetters = "senselessness".count({ it == 's' })
     print(numLetters)
 }
-*/
+
 
